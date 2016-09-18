5592aa8662052bb839460627797d8c5a419105a1
William Farnworth <wfarnworth@sis.tv>
2016-09-19 00:58:35 +0100

    Surpressed output and testing failure.  Added in correct systemctl usage

--- docker/install.sh
+++ docker/install.sh
@@ -3,27 +3,30 @@
 # Stolen from https://docs.docker.com/engine/installation/linux/centos/
 
 function destroy {
-        sudo yum -y remove docker-engine
-        rm -f /etc/yum.repos.d/docker.repo
+	echo -e "\nDocker failed to install sucessfully\n"
+	sudo systemctl stop docker > /dev/null 2>&1
+        sudo yum -y remove docker-engine > /dev/null 2>&1
+        sudo rm -f /etc/yum.repos.d/docker.repo > /dev/null 2>&1
 	exit 1
 }
 
 # Kernal must be version 3.10 or higher
 if [ `uname -r | cut -d\- -f1 | tr -d '.'` -lt 3100 ]; then
-	echo "Kernal version must be 3.10 or higher"
+	echo "\nKernal version must be 3.10 or higher\n"
 	exit 1
 fi
 
 # Must be running on a 64-bit architecture
-if [ `/usr/bin/uname -r | cut -d\_ -f2` -ne 64 ]; then
-        echo "Must be running a 64 bit architecture"
+if [ `uname -r | cut -d\_ -f2` -ne 64 ]; then
+        echo "\nMust be running a 64 bit architecture\n"
         exit 1
 fi
 
 # Make sure we have latest packages
-sudo yum -y update
+sudo yum -y update > /dev/null 2>&1
 
 # Install the Docker repo
+{
 sudo tee /etc/yum.repos.d/docker.repo <<-'EOF'
 [dockerrepo]
 name=Docker Repository
@@ -32,26 +35,26 @@ enabled=1
 gpgcheck=1
 gpgkey=https://yum.dockerproject.org/gpg
 EOF
+} > /dev/null 2>&1
 
 # Install Docker engine
-sudo yum install -y docker-engine
+sudo yum install -y docker-engine > /dev/null 2>&1
 
 # Start the service
-sudo service docker start
+sudo systemctl start docker > /dev/null 2>&1
 
 # Check if service started correctly, if not remove
-if [ '$?' -ne '0' ]
-	echo "Failed to start the Docker engine"
+if [ $? -ne 0 ]; then
 	destroy
 fi
 
 # Run Hello World Docker image to verify installation
-sudo docker run hello-world | grep "Hello from Docker"
+sudo docker run hello-world | grep "Hello from Docker" > /dev/null 2>&1
 
 # Check if image successfully ran
-if [ '$' -eq '0' ]
-	echo "Docker successfully installed"
+if [ $? -eq 0 ]; then
+	echo -e "\nDocker successfully installed\n"
+	exit 0
 else
-	echo "Docker failed to install sucessfully"
 	destroy
 fi
