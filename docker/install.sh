c2b29986ad562e8a27aee6135c9dc672db702ac9
Will Farnworth <wfarn@hotmail.co.uk>
2016-10-27 11:46:46 +0100

    Incorrect docker-compose location

--- docker/install.sh
+++ docker/install.sh
@@ -83,7 +83,7 @@ if [ $? -eq 0 ]; then
 fi
 
 # Install docker-compose
-sudo curl -L https://github.com/docker/machine/releases/download/v0.8.2/docker-machine-`uname -s`-`uname -m` >/usr/local/bin/docker-machine && \ chmod +x /usr/local/bin/docker-machine
+curl -L https://github.com/docker/compose/releases/download/1.9.0-rc1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose && chmod +x /usr/local/bin/docker-compose
 
 # Check compose installed correctly
 if [[ $(docker-compose --version | grep "docker-compose") ]]; then
