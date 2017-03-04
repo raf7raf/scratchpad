e3cc6bb521b4bceaeec3e55fb0506548b22591ba
William Farnworth <wfarnworth@sis.tv>
2017-03-04 00:53:30 +0000

    Devloping private/public network interfaces and networking further

--- jenkins_swarm_tf/jenkins_swarm.tf
+++ jenkins_swarm_tf/jenkins_swarm.tf
@@ -15,6 +15,36 @@ resource "aws_subnet" "jenkis_swarm" {
   availability_zone = "eu-west-1a"
 }
 
+resource "aws_network_interface" "master_public_nic" {
+    subnet_id = "${aws_subnet.public_a.id}"
+    private_ips = ["10.0.0.50"]
+    security_groups = ["${aws_security_group.web.id}"]
+    attachment {
+        instance = "${aws_instance.test.id}"
+        device_index = 1
+    }
+}
+
+resource "aws_network_interface" "master_priv_nic" {
+    subnet_id = "${aws_subnet.public_a.id}"
+    private_ips = ["10.0.0.50"]
+    security_groups = ["${aws_security_group.web.id}"]
+    attachment {
+        instance = "${aws_instance.test.id}"
+        device_index = 1
+    }
+}
+
+resource "aws_network_interface" "slave_nic${count.index}" {
+    subnet_id = "${aws_subnet.public_a.id}"
+    private_ips = ["10.0.0.50"]
+    security_groups = ["${aws_security_group.web.id}"]
+    attachment {
+        instance = "${aws_instance.test.${count.index}}"
+        device_index = 1
+    }
+}
+
 resource "aws_security_group" "jenkins_master" {
   vpc_id = "${aws_vpc.jenkins_swarm.id}"
   name = "jenkins_master"
@@ -73,6 +103,15 @@ resource "aws_instance" "master" {
     ansibleNodeName = "jenkins_master"
   }
 
+  # https://wiki.jenkins-ci.org/display/JENKINS/Installing+Jenkins+on+Ubuntu
+  provisioner "remote-exec" {
+      inline = [
+      "sudo wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -",
+      "sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'",
+      "sudo apt-get update",
+      "sudo apt-get install jenkins"
+      ]
+  }
 }
 
 resource "aws_instance" "slave" {
@@ -85,4 +124,10 @@ resource "aws_instance" "slave" {
     ansibleNodeType = "jenkins_slave"
     ansibleNodeName = "jenkins_slave${count.index}"
   }
+  provisioner "remote-exec" {
+        inline = [
+        "wget -O ~/swarm-client.jar https://repo.jenkins-ci.org/releases/org/jenkins-ci/plugins/swarm-client/3.3/swarm-client-3.3.jar",
+	"java -master http://master.public:dns:8080 -username ${var.jenkins_user} -password  ${var.jenkins_pass} -name slave${count.index} -executors 2 -jar ~/swarm-client.jar"
+        ]
+    }
 }
